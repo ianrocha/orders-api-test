81f4415839d571800b0f1b09b0303e08d000bff7
Ian rocha <ianbritosr@gmail.com>
2020-10-10 15:22:00 -0300

    Update production settings

--- orderapi/settings/production.py
+++ orderapi/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['order-maker.herokuapp.com']
+ALLOWED_HOSTS = ['orders-api-test.herokuapp.com']
 
 # Application definition
 
