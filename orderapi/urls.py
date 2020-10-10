05de7ebbb889b2655216597c8b9af470139f6ca2
Ian rocha <ianbritosr@gmail.com>
2020-10-09 22:43:09 -0300

    Add clients endpoint

--- orderapi/urls.py
+++ orderapi/urls.py
@@ -20,9 +20,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_swagger.views import get_swagger_view
 
+from clients.viewset import ClientViewSet
 from products.viewset import ProductViewSet
 
 router = DefaultRouter()
+router.register(r'client', ClientViewSet, basename='ClientViewSet')
 router.register(r'product', ProductViewSet, basename='ProductViewSet')
 schema_view = get_swagger_view(title='OrderApi')
 
