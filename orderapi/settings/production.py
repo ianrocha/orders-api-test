ad4cf83e26c5abe021e927903a44963075d74c26
Ian rocha <ianbritosr@gmail.com>
2020-10-10 17:22:29 -0300

    Update prod settings

--- orderapi/settings/production.py
+++ orderapi/settings/production.py
@@ -125,13 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
-MEDIA_URL = '/media/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
-)
